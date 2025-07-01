d1930d20101ba48a1dd55dd20dad8495800eabcf
AnujThakur-web <kumaranujkk@gmail.com>
2025-07-01 23:08:27 +0530

    maine delhi pulice ki story likhi hai padh lo sabi log

--- Environment/main.tf
+++ Environment/main.tf
@@ -4,10 +4,19 @@ module "azurerm_resource_group" {
   resource_group_location = "West US"
 }
 
+module "azurerm_resource_group2" {
+  source                  = "../Modules/azurerm_resource_group"
+  resource_group_name     = "todo_app_rg2"
+  resource_group_location = "West US"
+}
+
+
 module "azurerm_virtual_network" {
   depends_on           = [module.azurerm_resource_group]
   source               = "../Modules/azurerm_virtual_network"
   virtual_network_name = "todoapp_vnet"
+
+
   address_space        = ["10.0.0.0/16"]
   location             = "West US"
   resource_group_name  = "todo_app_rg"
@@ -139,3 +148,22 @@ module "vm_password_secret" {
   resource_group_name = "todo_app_rg"
 }
 
+
+
+# Story: 1
+# A brutal gangrape in a moving bus shocked the nation. Delhi Police formed multiple teams to track the culprits. 
+# In just 72 hours, all accused were arrested from different states. Police used CCTV footage, 
+# bus records, and mobile tracking. It was one of the fastest investigations in such a high-profile case.
+
+# Story:2
+# During the terrorist attacks in Mumbai, the local police, especially officers like Tukaram Omble, 
+# showed extreme bravery. Omble, unarmed, caught Ajmal Kasab alive, which was crucial for the investigation. 
+# His sacrifice saved many lives.
+
+# Story (short):
+# In a posh colony, a diamond necklace vanished during a kitty party. 
+# Everyone blamed the house help. Inspector Vikram observed that the dog hadn’t 
+# barked at the thief — meaning the thief was someone familiar. After careful
+#  questioning and fingerprint scanning on the glass, it turned out the neighbor’s daughter stole it
+#   to pay off debts. 
+# Case closed with logic, not force.
