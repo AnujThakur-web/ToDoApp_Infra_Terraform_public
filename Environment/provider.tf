df02e2c5fa8d0023e125264d1997d9610eb70e8c
AnujThakur-web <kumaranujkk@gmail.com>
2025-07-03 19:00:49 +0530

    remove story

--- Environment/provider.tf
+++ Environment/provider.tf
@@ -1,13 +1,3 @@
-# Kahani 1947 ke Partition of India ke dauraan set hai.
-# Tara Singh, ek Sikh truck driver, ek Hindu-Sikh refugee camp mein kaam karta hai.
-# Wahaan uski mulaqat hoti hai Sakeena Ali se, ek Muslim ladki jo Pakistan se bhatak kar Hindustan aa gayi hoti hai.
-# Dange ke beech Tara Sakeena ki jaan bachata hai.
-# Dheere-dheere dono ek doosre se mohabbat kar baithte hain.
-# Tara Sakeena se shaadi kar leta hai, Sakeena Hindu ban kar uske saath rehne lagti hai.
-# Unka ek beta bhi hota hai – Jeet.
-# Ek din Sakeena ko pata chalta hai ke uske Abbu (Ashraf Ali) Pakistan mein zinda hain
-
-
 terraform {
   required_providers {
     azurerm = {
